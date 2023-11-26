38a5ff26ea6317eab181417d26f1d27d8c5ff594
Krzysztof Liszka <krzysztof.liszkaa@wp.pl>
2023-11-26 16:40:35 +0100

    add: users module backend

---
+++ PracaInzynierskaAPI/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PracaInzynierskaAPI.DTOs;
+using PracaInzynierskaAPI.Models;
+using PracaInzynierskaAPI.Services;
+using PracaInzynierskaAPI.Services.Interfaces;
+
+namespace PracaInzynierskaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IBaseItemService<User> _baseItemService;
+
+        public UsersController(IMapper mapper, IBaseItemService<User> baseItemService)
+        {
+            _mapper = mapper;
+            _baseItemService = baseItemService;
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<List<WorkerDto>> GetAllUserItems()
+        {
+            var items = await _baseItemService.GetAllItemsAsync();
+            var mappedItems = _mapper.Map<List<WorkerDto>>(items);
+
+            return mappedItems;
+        }
+
+        [HttpDelete("DeleteUser/{id}")]
+        public async Task<IActionResult> DeleteImage(Guid id)
+        {
+            await _baseItemService.DeleteItemAsync(id);
+            return Ok();
+        }
+    }
+}
